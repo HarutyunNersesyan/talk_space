43c8981cd0f18e42e4cc153f81cb279a05f311e2
HARUT_037 <harut.037@mail.ru>
2024-09-29 21:42:41 +0400

    TS-1 Created project and set up configuration.

---
+++ src/main/java/com/talk_space/TalkSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.talk_space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talk_space/TalkSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talk_space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
