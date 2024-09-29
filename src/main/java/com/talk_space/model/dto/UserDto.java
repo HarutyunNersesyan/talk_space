065a89bbed0bf88f3d4d0ea7b982837af3480a2d
HARUT_037 <harut.037@mail.ru>
2024-09-29 21:51:43 +0400

    TS-7 Added security logic

---
+++ src/main/java/com/talk_space/model/dto/UserDto.java
@@ -0,0 +1,15 @@
+package com.talk_space.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDto {
+    private String email;
+    private String password;
+}
