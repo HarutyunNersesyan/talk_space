bded997a9ffce1a1729e204a1e975774c2c68161
HARUT_037 <harut.037@mail.ru>
2024-09-29 21:48:45 +0400

    TS-5 Added service layer and CRUD functionality

---
+++ src/main/java/com/talk_space/service/HobbyService.java
@@ -0,0 +1,45 @@
+package com.talk_space.service;
+
+
+import com.talk_space.model.domain.Hobby;
+import com.talk_space.repository.HobbyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class HobbyService {
+
+    private final HobbyRepository hobbyRepository;
+
+    public Hobby save(Hobby hobby){
+       return hobbyRepository.save(hobby);
+    }
+
+    public Optional<Hobby> getHobbyById(Long id){
+        return hobbyRepository.findById(id);
+    }
+
+    public List<Hobby> getAll(){
+        return hobbyRepository.findAll();
+    }
+
+    public Hobby update(Hobby hobby){
+        return hobbyRepository.save(hobby);
+    }
+
+    public Optional<Hobby> getHobbyByName(String hobbyName){
+        return hobbyRepository.findHobbyByName(hobbyName);
+
+    }
+
+    public void deleteHobbyById(Long id){
+        hobbyRepository.deleteById(id);
+    }
+
+    public void deleteHobbyByName(String hobbyName){
+        hobbyRepository.deleteHobbyByName(hobbyName);
+    }
+}
---
+++ src/main/java/com/talk_space/service/UserService.java
@@ -0,0 +1,162 @@
+package com.talk_space.service;
+
+
+import com.talk_space.model.domain.Hobby;
+import com.talk_space.model.domain.User;
+import com.talk_space.model.dto.ForgotPassword;
+import com.talk_space.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    private final AuthenticationManager authenticationManager;
+
+
+    private final PasswordEncoder passwordEncoder;
+
+
+
+
+    @Autowired
+    public UserService(UserRepository userRepository, @Lazy AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
+
+
+    }
+
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public User update(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public String hashPassword(String newPassword) {
+        return passwordEncoder.encode(newPassword);
+    }
+
+    public void forgotPassword(String email, String newPassword) {
+        Optional<User> user = userRepository.findUserByEmail(email);
+        if (user.isEmpty()) {
+            throw new IllegalArgumentException("No user found with this email address.");
+        }
+
+        user.get().setPassword(hashPassword(newPassword));
+        userRepository.save(user.get());
+    }
+
+    public ResponseEntity<String> changePassword(String email, String oldPassword, String newPassword, String newPasswordRepeat) {
+
+        Optional<User> optionalUser = userRepository.findUserByEmail(email);
+        if (optionalUser.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+
+        User user = optionalUser.get();
+
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, oldPassword));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Old password is incorrect.");
+        }
+
+        if (passwordEncoder.matches(newPassword, user.getPassword())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("New password cannot be the same as the current password.");
+        }
+
+        if (!newPassword.equals(newPasswordRepeat)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("New passwords do not match.");
+        }
+
+        user.setPassword(hashPassword(newPassword));
+        userRepository.save(user);
+
+        return ResponseEntity.ok("Password updated successfully.");
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findUserByEmail(email).
+                orElseThrow(() -> new RuntimeException("Customer not found with id: " + email));
+
+        String role = user.getRole().name();
+
+        UserDetails userDetails = new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singleton(new SimpleGrantedAuthority(role)));
+        return userDetails;
+    }
+
+    public Optional<User> findUserByEmail(String email) {
+        return userRepository.findUserByEmail(email);
+    }
+
+    public ResponseEntity<String> forgotPassword(ForgotPassword forgotPassword) {
+
+        Optional<User> optionalUser = findUserByEmail(forgotPassword.getEmail());
+
+        if (optionalUser.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+
+        User existingUser = optionalUser.get();
+
+        if (!existingUser.getPin().equals(forgotPassword.getPin())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid PIN.");
+        }
+
+        if (passwordEncoder.matches(forgotPassword.getNewPassword(), existingUser.getPassword())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("New password cannot be the same as the current password.");
+        }
+
+        existingUser.setPassword(hashPassword(forgotPassword.getNewPassword()));
+        save(existingUser);
+
+        return ResponseEntity.ok("Password updated successfully.");
+    }
+
+    public void deleteAccount(String password){
+        userRepository.deleteUserByPassword(password);
+    }
+
+
+
+}
